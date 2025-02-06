27b3fe38bef5842059de0226e43344da5a371d9e
Divya <divyawnd@gmail.com>
2025-02-06 10:53:03 +0530

    configured router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import "./App.css";
+import Movies from "./components/Movies";
 import Navbar from "./components/Navbar";
+import WatchList from "./components/WatchList";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <main>
-      <Navbar/>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Movies />} />
+          <Route path="/watch-list" element={<WatchList />} />
+        </Routes>
+      </BrowserRouter>
     </main>
   );
 }
