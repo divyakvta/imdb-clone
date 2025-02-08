76d2497165ca11e9d60c907c98f6de3f71f2e481
Divya <divyawnd@gmail.com>
2025-02-08 21:58:30 +0530

    Implemented watchlist add and remove properties

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import Banner from "./components/Banner";
 import Movies from "./components/Movies";
@@ -6,6 +7,31 @@ import WatchList from "./components/WatchList";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
+
+  const [watchList, setWatchList] = useState([]);
+
+  const addToWatchList = (movieObj) => {
+    let newWatchList = [...watchList, movieObj];
+    localStorage.setItem('imdb_clone', JSON.stringify(newWatchList))
+    setWatchList(newWatchList)
+    console.log(newWatchList)
+  }
+
+  const removeFromWatchList =(movieObj) => {
+    let filteredWatchList = watchList.filter(movie => {
+      return movie.id !== movieObj.id
+    })
+    setWatchList(filteredWatchList)
+    console.log(filteredWatchList )
+  }
+
+  useEffect(() => {
+    const moviesFromLocalStorage = localStorage.getItem('imdb_clone');
+    if (moviesFromLocalStorage) {
+      setWatchList(JSON.parse(moviesFromLocalStorage));
+    }
+  }, []);
+
   return (
     <main className="mb-20">
       <BrowserRouter>
@@ -16,11 +42,11 @@ function App() {
             path="/"
             element={
               <>
-                <Banner /> <Movies />
+                <Banner /> <Movies watchList={watchList} addToWatchList={addToWatchList} removeFromWatchList={removeFromWatchList} />
               </>
             }
           />
-          <Route path="/watch-list" element={<WatchList />} />
+          <Route path="/watch-list" element={<WatchList watchList={watchList} />} />
         </Routes>
       </BrowserRouter>
     </main>
